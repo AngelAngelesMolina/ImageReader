e5d981a6ea0cc2fb23419c60bbad34637a5bf7c4
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-08-05 20:27:59 -0600

    initial views

---
+++ app/src/main/java/com/jaamcoding/imagereader/viewmodel/ScannerViewModel.kt
@@ -0,0 +1,6 @@
+package com.jaamcoding.imagereader.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ScannerViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/jaamcoding/imagereader/views/CamaraView.kt
@@ -0,0 +1,13 @@
+package com.jaamcoding.imagereader.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.jaamcoding.imagereader.viewmodel.ScannerViewModel
+
+@Composable
+fun CamaraView(
+    vm: ScannerViewModel
+) {
+    Text(text = "CamaraView")
+
+}
