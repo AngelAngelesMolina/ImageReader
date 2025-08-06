c993b3a4f4f036ba44b83716eb44d2dfb2fe42c6
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-08-05 20:38:46 -0600

    Access to device's gallery

--- app/src/main/java/com/jaamcoding/imagereader/views/GalleryView.kt
+++ app/src/main/java/com/jaamcoding/imagereader/views/GalleryView.kt
@@ -1,13 +1,54 @@
 package com.jaamcoding.imagereader.views
 
-import androidx.compose.material3.Text
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.jaamcoding.imagereader.R
 import com.jaamcoding.imagereader.viewmodel.ScannerViewModel
 
 @Composable
 fun GalleryView(
     vm: ScannerViewModel
 ) {
-    Text(text = "GalleryView")
+    val context = LocalContext.current
+    var image: Any? by remember { mutableStateOf(R.drawable.ic_gallery) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            image = it
+        } else {
+            Toast.makeText(context, "No image selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            modifier = Modifier.clickable {
+                photoPicker.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }.padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
+
+    }
+
 
 }
