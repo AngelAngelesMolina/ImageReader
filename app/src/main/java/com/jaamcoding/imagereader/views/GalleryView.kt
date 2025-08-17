bf2d2ea07538cc90e2f73b788bf823aa5e90442e
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-08-16 19:22:45 -0600

    Gallery view

--- app/src/main/java/com/jaamcoding/imagereader/views/GalleryView.kt
+++ app/src/main/java/com/jaamcoding/imagereader/views/GalleryView.kt
@@ -7,8 +7,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,7 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.jaamcoding.imagereader.R
@@ -27,26 +36,45 @@ fun GalleryView(
     vm: ScannerViewModel
 ) {
     val context = LocalContext.current
+    val clipboard = LocalClipboardManager.current
     var image: Any? by remember { mutableStateOf(R.drawable.ic_gallery) }
     val photoPicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia()
     ) {
         if (it != null) {
             image = it
+            vm.onRecognizeText(image, context)
         } else {
-            Toast.makeText(context, "No image selected", Toast.LENGTH_SHORT).show()
+            vm.showToast(context, "No image selected")
         }
     }
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
-            modifier = Modifier.clickable {
-                photoPicker.launch(
-                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                )
-            }.padding(16.dp, 8.dp),
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }
+                .padding(16.dp, 8.dp),
             painter = rememberAsyncImagePainter(image),
             contentDescription = null
         )
+        Spacer(modifier = Modifier.height(25.dp))
+
+        val scrollState = rememberScrollState()
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipboard.setText(AnnotatedString(vm.recognizedText))
+                    vm.showToast(context, "Texto copiado al portapapeles")
+                },
+            text = vm.recognizedText,
+            textAlign = TextAlign.Center
+        )
+
 
     }
 
